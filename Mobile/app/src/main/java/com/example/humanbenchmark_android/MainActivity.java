93bffbd773ef32451ed8c07285182124dced83bf
RDDcat <yukmaro@gmail.com>
2021-07-16 19:04:57 +0900

    [ADD] android main pages and webViews
    안드로이드 웹뷰 8개 생성

--- Mobile/app/src/main/java/com/example/humanbenchmark_android/MainActivity.java
+++ Mobile/app/src/main/java/com/example/humanbenchmark_android/MainActivity.java
@@ -2,13 +2,74 @@ package com.example.humanbenchmark_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button aimTestButton;
+    private Button chimpTestButton;
+    private Button numberMemoryTestButton;
+    private Button reactionTestButton;
+    private Button sequenceMemoryTestButton;
+    private Button typingTestButton;
+    private Button verbalMemoryTestButton;
+    private Button visualMemoryTestButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        aimTestButton =  findViewById(R.id.aimTestButton);
+        chimpTestButton =  findViewById(R.id.chimpTestButton);
+        numberMemoryTestButton =  findViewById(R.id.numberMemoryTestButton);
+        reactionTestButton =  findViewById(R.id.reactionTestButton);
+        sequenceMemoryTestButton =  findViewById(R.id.sequenceMemoryTestButton);
+        typingTestButton =  findViewById(R.id.typingTestButton);
+        verbalMemoryTestButton =  findViewById(R.id.verbalMemoryTestButton);
+        visualMemoryTestButton =  findViewById(R.id.visualMemoryTestButton);
+
+        aimTestButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), AimTestActivity.class);
+            startActivity(intent);
+        });
+
+        chimpTestButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), ChimpTestActivity.class);
+            startActivity(intent);
+        });
+
+        numberMemoryTestButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), NumberMemoryTestActivity.class);
+            startActivity(intent);
+        });
+
+        reactionTestButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), ReactionTestActivity.class);
+            startActivity(intent);
+        });
+
+        sequenceMemoryTestButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), SequenceMemoryTestActivity.class);
+            startActivity(intent);
+        });
+
+        typingTestButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), TypingTestActivity.class);
+            startActivity(intent);
+        });
+
+        verbalMemoryTestButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), VerbalMemoryTestActivity.class);
+            startActivity(intent);
+        });
+
+        visualMemoryTestButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), VisualMemoryTestActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
---
+++ Mobile/app/src/main/java/com/example/humanbenchmark_android/SequenceMemoryTestActivity.java
@@ -0,0 +1,33 @@
+package com.example.humanbenchmark_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class SequenceMemoryTestActivity extends AppCompatActivity {
+    private WebView webView;
+    private WebSettings webSettings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sequence_memory_test);
+
+
+        webView = (WebView) findViewById(R.id.webViewSequenceMemoryTest);
+
+        webView.setWebViewClient(new WebViewClient());
+        webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        webSettings.setDomStorageEnabled(true);
+        webView.loadUrl("http://118.67.129.193/sequence-memory-test/smt_start.jsp");
+
+    }
+}
