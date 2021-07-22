c1665f64d41ec4832ea4de72dc3568f51672ed5c
RDDcat <yukmaro@gmail.com>
2021-07-22 13:58:49 +0900

    [UPDATE] userid data from MainActivity to AimTestActivity
    uid aimtest 액티비티에 전송

--- Mobile/app/src/main/java/com/example/humanbenchmark_android/AimTestActivity.java
+++ Mobile/app/src/main/java/com/example/humanbenchmark_android/AimTestActivity.java
@@ -1,15 +1,20 @@
 package com.example.humanbenchmark_android;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class AimTestActivity extends AppCompatActivity {
     private WebView webView;
     private WebSettings webSettings;
+
+    // 로그인 id정보 받아오기
+    private Intent intent = getIntent();
+    private String uid = intent.getStringExtra("uid");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +32,6 @@ public class AimTestActivity extends AppCompatActivity {
         webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
         webSettings.setDomStorageEnabled(true);
         webView.loadUrl("http://118.67.129.193/aim-test/at_start.jsp");
-
 
     }
 }
--- Mobile/app/src/main/java/com/example/humanbenchmark_android/MainActivity.java
+++ Mobile/app/src/main/java/com/example/humanbenchmark_android/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.humanbenchmark_android;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 
@@ -25,11 +25,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent i =getIntent();
+        Intent i = getIntent();
         String nickName = i.getStringExtra("nickName");
         String photoURL = i.getStringExtra("photoURL");
         String uid = i.getStringExtra("uid");
-
 
 
 
